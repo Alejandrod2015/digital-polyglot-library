43f58643bf3bb032978d33677f60e2b09fdcba9e
Alejandrod2015 <delcarpio321@gmail.com>
2025-10-02 18:21:39 +0200

    Update

--- src/app/books/[bookId]/stories/layout.tsx
+++ src/app/books/[bookId]/stories/layout.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import PlayerWrapper from "@/components/PlayerWrapper";
-
 export default function StoriesLayout({
   children,
 }: {
@@ -10,11 +8,6 @@ export default function StoriesLayout({
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex-1">{children}</main>
-
-      {/* Player solo en páginas de stories */}
-      <div className="fixed bottom-0 left-0 right-0 z-20 md:ml-64">
-        <PlayerWrapper />
-      </div>
     </div>
   );
 }
