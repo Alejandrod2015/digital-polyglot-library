711e29948a1c904d97d516334d2591be3bf9d92a
Alejandrod2015 <delcarpio321@gmail.com>
2025-10-11 17:21:31 +0200

    Update

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,11 +3,11 @@
  * Compatible con Next.js (local / Vercel) y Sanity Cloud.
  */
 
-import { createClient } from 'next-sanity'
+import { createClient } from 'next-sanity';
 
 export const client = createClient({
-  projectId: '9u7ilulp',
-  dataset: 'production',
-  apiVersion: '2025-10-05',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-05-01',
   useCdn: true,
-})
+});
