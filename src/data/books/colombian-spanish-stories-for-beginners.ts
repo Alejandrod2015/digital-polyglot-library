ce49aaed8e9908a0c59c3fcdb718144a5b61e565
Alejandrod2015 <delcarpio321@gmail.com>
2026-01-20 00:26:48 +0100

    chore: publish book in development

---
+++ src/data/books/colombian-spanish-stories-for-beginners.ts
@@ -0,0 +1,18 @@
+import { Book } from "@/types/books";
+
+export const colombianspanishstoriesforbeginners: Book = {
+  id: "colombian-spanish-stories-for-beginners",
+  slug: "colombian-spanish-stories-for-beginners",
+  title: "Colombian Spanish Stories for Beginners",
+  description: "20 Fun Tales to Learn Colombian Expressions and Boost Your Spanish Fluency",
+  cover: "https://cdn.sanity.io/images/9u7ilulp/production/0577a9b9deba611ca2af702a12ac07aa3aa64e9f-6740x9505.jpg",
+  theme: [],
+  level: "beginner",
+  language: "spanish",
+  region: "colombia",
+  topic: "",
+  formality: "neutral",
+  audioFolder: "",
+  storeUrl: "",
+  stories: []
+};
--- src/data/books/index.ts
+++ src/data/books/index.ts
@@ -1,8 +1,10 @@
 import type { Book } from "@/types/books";
 import { spanishshortstorieson20mexicanwonders } from "./spanish-short-stories-on-20-mexican-wonders";
+import { colombianspanishstoriesforbeginners } from "./colombian-spanish-stories-for-beginners";
 import { shortstoriesincolombianspanish } from "./short-stories-in-colombian-spanish";
 
 export const books: Record<string, Book> = {
   [spanishshortstorieson20mexicanwonders.id]: spanishshortstorieson20mexicanwonders,
+  [colombianspanishstoriesforbeginners.id]: colombianspanishstoriesforbeginners,
   [shortstoriesincolombianspanish.id]: shortstoriesincolombianspanish,
 };
