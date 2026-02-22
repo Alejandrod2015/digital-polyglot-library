408297c8e2872a85c70df3f662aad9d1311d821b
Alejandrod2015 <delcarpio321@gmail.com>
2026-02-22 18:05:33 +0100

    Export from Sanity

--- src/data/books/index.ts
+++ src/data/books/index.ts
@@ -2,9 +2,11 @@ import type { Book } from "@/types/books";
 import { spanishshortstorieson20mexicanwonders } from "./spanish-short-stories-on-20-mexican-wonders";
 import { colombianspanishstoriesforbeginners } from "./colombian-spanish-stories-for-beginners";
 import { shortstoriesincolombianspanish } from "./short-stories-in-colombian-spanish";
+import { colloquialgermanstories } from "./colloquial-german-stories";
 
 export const books: Record<string, Book> = {
   [spanishshortstorieson20mexicanwonders.id]: spanishshortstorieson20mexicanwonders,
   [colombianspanishstoriesforbeginners.id]: colombianspanishstoriesforbeginners,
   [shortstoriesincolombianspanish.id]: shortstoriesincolombianspanish,
+  [colloquialgermanstories.id]: colloquialgermanstories,
 };
